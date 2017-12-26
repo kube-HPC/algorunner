e363c44022ccac21ca52f9f21eee1cc6bb1538d0
Nassi Harel <nassi9@gmail.com>
2017-12-26 17:01:31 +0200

    improve eval alg with function and inputs

--- config/main/config.base.js
+++ config/main/config.base.js
@@ -6,8 +6,8 @@ config.serviceName = 'algorunner';
 config.workerCommunication = {
     adapterName: 'socket',
     config: {
-        connection:{
-            port: process.env.WORKER_SOCKET_PORT || 3000,
+        connection: {
+            port: process.env.WORKER_SOCKET_PORT || 8000,
             host: process.env.WORKER_SOCKET_HOST || "localhost"
         }
     }
