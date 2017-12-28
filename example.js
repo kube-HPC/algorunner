623c74dfe48d26be11e6f3d43bf50468be9c40b7
yehiyam <yehiyam@gmail.com>
2017-12-28 17:04:44 +0200

    add example

---
+++ example.js
@@ -0,0 +1,50 @@
+const store = {
+    "name": "bigevaltest",
+    "nodes": [
+        {
+            "nodeName": "evaladd",
+            "algorithmName": "eval-alg",
+            "input": [
+                [
+                    "(input,require)=> {",
+                    "const result= input[0][0]+input[0][1]",
+                    "return [result];}"
+                ],
+                "@flowInput.addInput"
+            ]
+        },
+        {
+            "nodeName": "evalmul",
+            "algorithmName": "eval-alg",
+            "input": [
+                [
+                    "(input,require)=> {",
+                    "const result = input[0][0]*input[1].results[0]",
+                    "return result;}"
+                ],
+                "@flowInput.multInput",
+                "@evaladd"
+            ]
+        }
+    ],
+    "webhooks": {
+        "progress": "string",
+        "result": "string"
+    },
+    "options": {
+        "batchTolerance": 80,
+        "progressVerbosityLevel": "info"
+    }
+}
+
+const execute = {
+    "name": "bigevaltest",
+    "flowInput":{
+        "addInput":[4,5],
+        "multInput":[4]
+    },
+    "options": {
+        "batchTolerance": 60,
+        "progressVerbosityLevel": "info"
+    }
+}
