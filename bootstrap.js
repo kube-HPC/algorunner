6bf643f19e5c6b89a8a9f6a8a33d42d5fb90f641
Nassi Harel <nassi9@gmail.com>
2018-10-31 12:38:42 +0200

    fix loggings

--- bootstrap.js
+++ bootstrap.js
@@ -1,8 +1,6 @@
 const configIt = require('@hkube/config');
-const Logger = require('@hkube/logger');
 const componentName = require('./lib/consts/componentNames');
 const algorunner = require('./lib/algorunner');
-let log;
 
 const modules = [
     './lib/workerCommunication/workerCommunication.js'
@@ -11,11 +9,9 @@ const modules = [
 class Bootstrap {
     async init() {
         try {
-            const { main, logger } = configIt.load();
+            const { main } = configIt.load();
             this._handleErrors();
 
-            log = new Logger(main.serviceName, logger);
-            log.info('running application in ' + configIt.env() + ' environment', { component: componentName.MAIN });
             await Promise.all(modules.map(m => require(m).init(main)));
             await algorunner.init(main);
             return main;
@@ -26,34 +22,28 @@ class Bootstrap {
     }
 
     _onInitFailed(error) {
-        if (log) {
-            log.error(error.message, { component: componentName.MAIN }, error);
-            log.error(error);
-        }
-        else {
-            console.error(error.message);
-            console.error(error);
-        }
+        console.error(error.message);
+        console.error(error);
         process.exit(1);
     }
 
     _handleErrors() {
         process.on('exit', (code) => {
-            log.info('exit' + (code ? ' code ' + code : ''), { component: componentName.MAIN });
+            console.info('exit' + (code ? ' code ' + code : ''), { component: componentName.MAIN });
         });
         process.on('SIGINT', () => {
-            log.info('SIGINT', { component: componentName.MAIN });
+            console.info('SIGINT', { component: componentName.MAIN });
             process.exit(1);
         });
         process.on('SIGTERM', () => {
-            log.info('SIGTERM', { component: componentName.MAIN });
+            console.info('SIGTERM', { component: componentName.MAIN });
             process.exit(1);
         });
         process.on('unhandledRejection', (error) => {
-            log.error('unhandledRejection: ' + error.message, { component: componentName.MAIN }, error);
+            console.error('unhandledRejection: ' + error.message, { component: componentName.MAIN }, error);
         });
         process.on('uncaughtException', (error) => {
-            log.error('uncaughtException: ' + error.message, { component: componentName.MAIN }, error);
+            console.error('uncaughtException: ' + error.message, { component: componentName.MAIN }, error);
             process.exit(1);
         });
     }
