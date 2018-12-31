83fb774a43985938ba6477efdf56e00c1677dbd6
yehiyam <yehiyam@users.noreply.github.com>
2018-12-31 14:41:40 +0200

    revert port change

--- config/main/config.base.js
+++ config/main/config.base.js
@@ -7,7 +7,7 @@ config.workerCommunication = {
     adapterName: 'socket',
     config: {
         connection: {
-            port: process.env.WORKER_SOCKET_PORT || 6846,
+            port: process.env.WORKER_SOCKET_PORT || 3000,
             host: process.env.WORKER_SOCKET_HOST || "localhost",
             // optional url. If provided ignores the above host and port
             url: process.env.WORKER_SOCKET_URL || ''
