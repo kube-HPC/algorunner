16007e09f367813e568a21628ce19b25976055ac
Nassi Harel <nassi9@gmail.com>
2020-02-27 14:08:35 +0200

    add more encoding

--- config/main/config.base.js
+++ config/main/config.base.js
@@ -3,15 +3,20 @@ const { parseBool } = require(process.cwd() + '/lib/utils/formatters');
 const config = module.exports = {};
 
 config.serviceName = packageJson.name;
-
 config.adapter = process.env.WORKER_ALGORITHM_PROTOCOL || 'ws';
-config.binary = parseBool(process.env.WORKER_BINARY, false);
 config.maxPayload = process.env.WORKER_SOCKET_MAX_PAYLOAD_BYTES;
+
+config.capabilities = {
+    storage: 'byRaw',
+    encoding: 'json,bson'
+};
 
 config.socket = {
     port: process.env.WORKER_SOCKET_PORT || 3000,
     host: process.env.WORKER_SOCKET_HOST || 'localhost',
     protocol: process.env.WORKER_SOCKET_PROTOCOL || 'ws',
-    url: process.env.WORKER_SOCKET_URL || null
+    url: process.env.WORKER_SOCKET_URL || null,
+    binary: parseBool(process.env.WORKER_BINARY, false)
 };
 
+config.socket.encoding = config.socket.binary ? 'bson' : 'json';
