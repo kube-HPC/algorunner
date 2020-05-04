8a5d0b357c7eb79c6593ee4d8ae22dd7036fce6f
NassiHarel <nassi9@gmail.com>
2020-05-04 15:09:17 +0300

    feat: remove snyk

--- config/main/config.base.js
+++ config/main/config.base.js
@@ -2,14 +2,12 @@ const packageJson = require(process.cwd() + '/package.json');
 const config = module.exports = {};
 
 config.serviceName = packageJson.name;
-config.adapter = process.env.WORKER_ALGORITHM_PROTOCOL || 'ws';
 config.maxPayload = process.env.WORKER_SOCKET_MAX_PAYLOAD_BYTES;
-config.storage = process.env.STORAGE_MODE || 'byRaw';
 
 config.socket = {
     port: process.env.WORKER_SOCKET_PORT || 3000,
     host: process.env.WORKER_SOCKET_HOST || 'localhost',
     protocol: process.env.WORKER_SOCKET_PROTOCOL || 'ws',
     url: process.env.WORKER_SOCKET_URL || null,
-    encoding: process.env.WORKER_ENCODING || 'bson'
+    encoding: process.env.WORKER_ALGORITHM_ENCODING || 'bson'
 };
